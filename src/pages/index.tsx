fe9219390d8dae5dd6346bfcb8ab037af0f8eb91
Italo Almeida <italoalmeida89@gmail.com>
2022-03-12 12:23:44 -0300

    structure the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import { Header } from '../components/Header';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -26,6 +28,39 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
+const posts = [
+  {
+    uid: 'sdfsdf',
+    first_publication_date: '07/04/2021',
+    data: {
+      title: 'Título',
+      subtitle: 'Subtitulo ',
+      author: 'Autor',
+      readTime: '5'
+    }
+  },
+  {
+    uid: 'sdfsdf',
+    first_publication_date: '07/04/2021',
+    data: {
+      title: 'Título',
+      subtitle: 'Subtitulo ',
+      author: 'Autor',
+      readTime: '5'
+    }
+  },
+  {
+    uid: 'sdfsdf',
+    first_publication_date: '07/04/2021',
+    data: {
+      title: 'Título',
+      subtitle: 'Subtitulo ',
+      author: 'Autor',
+      readTime: '5'
+    }
+  }
+]
+
 export default function Home() {
   // TODO
   return (
@@ -35,6 +70,37 @@ export default function Home() {
       </Head>
 
       <Header />
+
+      <main className={commonStyles.container}>
+        <div className={styles.posts}>
+          {posts.map(post => (
+            <div key={post.uid} className={styles.posts} >
+              <Link href={`/post/${post.uid}`}>
+                <a>
+                  <strong>{post.data.title}</strong>
+                  <p>{post.data.subtitle}</p>
+                  <ul>
+                    <li>
+                      <FiCalendar />{post.first_publication_date}
+                    </li>
+                    <li>
+                      <FiUser />{post.data.author}
+                    </li>
+                    <li>
+                      <FiClock />{`${post.data.readTime} min`}
+                    </li>
+                  </ul>
+                </a>
+              </Link>
+            </div>
+          ))}
+        </div>
+
+        <button type="button">
+          Carregar mais posts
+        </button>
+
+      </main>
     </>
   )
 }
